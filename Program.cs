c94fd26dd94dc23826f5006f36e3d9e3bdaf4fa8
Wellington witaski <wellingtonwitaski@hotmail.com>
2021-08-23 15:51:05 -0300

    Exercicio 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ol_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+        }
+    }
+}
